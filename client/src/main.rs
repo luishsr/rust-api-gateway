80f129dff4fb964f33170de9f2e5e9b063c1445f
Luis Soares <80909424+luishsr@users.noreply.github.com>
2023-10-11 09:03:01 -0300

    Added Service Registry

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,4 @@
+use std::time::{Duration, SystemTime};
 use jsonwebtoken::{encode, Header, EncodingKey};
 use hyper::{Client, Request};
 use hyper_tls::HttpsConnector;
@@ -9,6 +10,7 @@ const SECRET_KEY: &'static str = "secret_key";  // Must match the secret in the 
 struct Claims {
     sub: String,
     iss: String,
+    exp: usize,
 }
 
 #[tokio::main]
@@ -16,9 +18,14 @@ async fn main() {
     let claims = Claims {
         sub: "1234567890".to_string(),
         iss: "my_issuer".to_string(),
+        exp: (SystemTime::now() + Duration::from_secs(3600))
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as usize, // Expires in 1 hour
     };
 
-    let token = encode(&Header::default(), &claims, &EncodingKey::from_secret(SECRET_KEY.as_ref())).expect("Token generation failed");
+    let token = encode(&Header::default(), &claims, &EncodingKey::from_secret(SECRET_KEY.as_ref())).unwrap();
+    println!("Token: {}", token);
 
     let client = {
         let https = HttpsConnector::new();
@@ -27,7 +34,7 @@ async fn main() {
 
     let request = Request::builder()
         .method("GET")
-        .uri("http://127.0.0.1:8080/service1")
+        .uri("http://127.0.0.1:8080/hello_service")
         .header("Authorization", token)
         .body(hyper::Body::empty())
         .expect("Request builder failed.");
